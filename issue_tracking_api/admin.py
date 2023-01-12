f54aca32cd061ef9f609fb5687f3be0715855d09
Florent SANIKA <flo.sanika@gmail.com>
2023-01-12 22:49:57 +0100

    [models] Creating models and migration

--- issue_tracking_api/admin.py
+++ issue_tracking_api/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+    )
+    search_fields = (
+        'first_name',
+        'last_name',
+    )
+
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'type',
+        'created_time',
+    )
+
+
+@admin.register(models.Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+        'permission',
+        'role',
+    )
+
+
+@admin.register(models.Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'tag',
+        'status',
+        'assignee',
+        'created_time',
+    )
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'issue',
+        'created_time',
+    )
--- issue_tracking_api/models.py
+++ issue_tracking_api/models.py
@@ -1,3 +1,141 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(verbose_name="Prénom", blank=False, max_length=128)
+    last_name = models.CharField(verbose_name="Nom de famille", blank=False, max_length=128)
+    email = models.EmailField(verbose_name="Adresse email", blank=False, max_length=128, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+
+class Project(models.Model):
+    class ProjectType(models.TextChoices):
+        PROJECT = 'project', ('Projet')
+        PRODUCT = 'product', ('Produit')
+        APPLICATION = 'application', ('Application')
+
+    project_id = models.BigAutoField(primary_key=True)
+    title = models.CharField(verbose_name="Titre", blank=False, max_length=128)
+    description = models.CharField(max_length=1024)
+    type = models.CharField(
+        max_length=128,
+        choices=ProjectType.choices,
+        default=ProjectType.PROJECT,
+    )
+    author_user = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        related_name="projects",
+        null=True,
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+
+class Contributor(models.Model):
+    class Permissions(models.TextChoices):
+        READ_ONLY = 'read', _('Lecture seule')
+        WRITE = 'write', _('Ecriture')
+        DELETE = 'delete', _('Suppression')
+
+    class Roles(models.TextChoices):
+        AUTHOR = 'author', _('Auteur')
+        MEMBER = 'member', _('Membre')
+        CONTRIBUTOR = 'contributor', _('Contributeur')
+
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        related_name="contributors",
+        null=True,
+    )
+    project = models.ForeignKey(
+        to=Project,
+        on_delete=models.CASCADE,
+        related_name="contributors",
+    )
+    permission = models.CharField(
+        max_length=128,
+        choices=Permissions.choices,
+        default=Permissions.READ_ONLY,
+    )
+    role = models.CharField(
+        max_length=128,
+        choices=Roles.choices,
+    )
+
+
+class Issue(models.Model):
+    class Tag(models.TextChoices):
+        BUG = 'bug', _('Bug')
+        FEATURE = 'feature', _('Amelioration')
+        TASK = 'task', _('Tache')
+
+    class Priority(models.TextChoices):
+        LOW = 'low', _('Faible')
+        MEDIUM = 'medium', _('Moyen')
+        HIGH = 'high', _('Eleve')
+
+    class Status(models.TextChoices):
+        TODO = 'todo', _('A faire')
+        IN_PROGRESS = 'in_progress', _('En cours')
+        DONE = 'done', _('Termine')
+
+    issue_id = models.BigAutoField(primary_key=True)
+    title = models.CharField(verbose_name="Titre", blank=False, max_length=128)
+    description = models.CharField(max_length=128)
+    tag = models.CharField(
+        verbose_name="Étiquette",
+        max_length=128,
+        choices=Tag.choices,
+    )
+    priority = models.CharField(
+        verbose_name="Priorité",
+        max_length=128,
+        choices=Priority.choices,
+    )
+    project = models.ForeignKey(
+        to=Project,
+        on_delete=models.CASCADE,
+        related_name="issues",
+    )
+    status = models.CharField(
+        max_length=128,
+        choices=Status.choices,
+    )
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        related_name="issues",
+        null=True,
+    )
+    assignee = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        related_name="assigned_issues",
+        null=True,
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    comment_id = models.BigAutoField(primary_key=True)
+    description = models.CharField(max_length=128)
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    issue = models.ForeignKey(
+        to=Issue,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
