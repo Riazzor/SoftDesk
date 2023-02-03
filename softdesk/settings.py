87ae1e517ee034dffc8b560fe59c928fa3a19baa
Florent SANIKA <flo.sanika@gmail.com>
2023-02-03 13:09:08 +0100

    [authentication] Rotate refresh token and blaklist old one

--- softdesk/settings.py
+++ softdesk/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_extensions",
     "issue_tracking_api",
+
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
@@ -136,8 +138,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
